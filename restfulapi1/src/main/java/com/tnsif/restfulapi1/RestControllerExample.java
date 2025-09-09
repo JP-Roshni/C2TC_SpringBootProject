9fdb05a9653f32e556caa5575b8fef5479b7c25f
JP-Roshni <roshnikalpu@gmail.com>
2025-09-09 18:19:39 +0530

    Restful API 1

---
+++ restfulapi1/src/main/java/com/tnsif/restfulapi1/RestControllerExample.java
@@ -0,0 +1,28 @@
+package com.tnsif.restfulapi1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestControllerExample {
+	@RequestMapping("/hello Coaders")
+	public String show()
+	{
+		return "Welcome";
+	}
+	@GetMapping("/hello")
+	public String greetings(@RequestParam String name)
+	{
+		return "Welcome "+name;
+	}
+	@GetMapping("/hello/{name}")
+	public String message(@PathVariable String name)
+	{
+		return "Welcome "+name;
+	}
+
+	
+}
